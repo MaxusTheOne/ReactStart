0e4e54aa51cd46135455a4f54047606431ea7511
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-10-02 08:45:54 +0200

    created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import { useEffect } from "react";
 import reactLogo from "./assets/react.svg";
+import Member from "./components/member";
+import useState from "react";
 
 function App() {
-    return (
-        <>
-            <header>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-                <h1>RACE Your React 🎉</h1>
-            </header>
-        </>
-    );
+  const [members, setMembers] = useState([]);
+  useEffect(() => {
+    const memberList = [{ firstName: "Mark", lastName: "Bil", email: "max@wow.dk" }];
+  }, []);
+
+  return (
+    <>
+      <header>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <h1>RACE Your React 🎉</h1>
+      </header>
+      <Member member={{ firstName: "Mark", lastName: "Bil", email: "max@wow.dk" }} />
+      <Member member={{ firstName: "Fillipa", lastName: "idk", email: "fil@ipa.dk" }} />
+    </>
+  );
 }
 
 export default App;
